7e4472be578bebcfd5d61b5966932625750a301c
zer-ef <homeboy.puka.crema@gmail.com>
2023-05-30 17:51:38 -0500

    Segundo commit

--- src/main/java/pe/chinchayore/prueba/HolaControler.java
+++ src/main/java/pe/chinchayore/prueba/HolaControler.java
@@ -7,7 +7,7 @@ import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HolaControler {
-    @GetMapping("")
+    @GetMapping("/hola-mundo")
     String index(){
         return "index";
     }
